fcc52f013d7bd80706979814516191ad6f401bcc
Matheus <matheus.bonore@gmail.com>
2019-05-03 12:56:28 -0300

    V1.1.0

--- src/app/autenticacao/autenticacao.routing.ts
+++ src/app/autenticacao/autenticacao.routing.ts
@@ -1,24 +1,25 @@
-import { Route } from "@angular/router";
+import { Routes } from "@angular/router";
 import { AutenticacaoComponent } from './autenticacao.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-export const autenticacaoRoutes :Route[] = [
-    {
-        path: '',        
-        component: AutenticacaoComponent,
-            children: [
-              {
-                path: '',
-                pathMatch: 'full',
-                redirectTo: 'entrar'
-              }, {
-                path: 'entrar',
-                component: EntrarComponent
-              }, {
-                path: 'cadastrar',
-                component: CadastrarComponent
-              }
-            ]
-    }
-]
+export const autenticacaoRoutes: Routes = [
+  {
+    path: '',        
+    component: AutenticacaoComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'entrar'
+      }, {
+        path: 'entrar',
+        component: EntrarComponent
+      }, {
+        path: 'cadastrar',
+        component: CadastrarComponent
+      }
+    ]
+  }
+];
+
