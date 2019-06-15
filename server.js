9b4c327b3bbe489bee2ab15419388488fd7ec956
Matheus <matheus.bonore@gmail.com>
2019-06-14 21:26:09 -0300

    Ajuste do arquivo server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/dist"));
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
